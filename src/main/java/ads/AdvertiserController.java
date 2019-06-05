a8953eca8326e957284978e262feaa429fbddd13
Cyrus Mahdavi <cyrusmahdavi@cyruss-mbp.ccu.clearchannel.com>
2019-06-05 14:53:42 -0500

    delete added

--- src/main/java/ads/AdvertiserController.java
+++ src/main/java/ads/AdvertiserController.java
@@ -1,6 +1,5 @@
 package ads;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +7,11 @@ import java.sql.*;
 
 @RestController
 public class AdvertiserController{
-        private String name,contactName;
-        private long creditLimit;
 
-        @RequestMapping("/api/advertiser")
+        @RequestMapping("/api/advertiser") //site to add a new advertiser to the database
         public String getName(@RequestParam(name = "name", defaultValue = "Name") String name,
                               @RequestParam(name = "contactName",defaultValue = "use name") String contactName,
                               @RequestParam(name = "creditLimit", defaultValue = "0") long creditLimit) throws Exception{
-            this.name = name;
-            this.contactName = contactName;
-            this.creditLimit = creditLimit;
 
             Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa","");
 
@@ -26,21 +20,28 @@ public class AdvertiserController{
                     name + "\', \'" + contactName + "\', " + creditLimit + ")");
             ResultSet rs = stmt.executeQuery("SELECT name FROM advertisers");
             String s= "";
-            while(rs.next())    s+="  " + rs.getString(1);
+            while(rs.next())
+                s+="  " + rs.getString(1);
             conn.close();
             return s;
         }
-        @RequestMapping("/api/checkcredits")
+        @RequestMapping("/api/checkcredits") //site to check if advertiser has enough credits
         public String checkCredits(@RequestParam(name = "name")String name) throws Exception{
             boolean enough;
-            System.out.println(name);
             Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa","");
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM advertisers WHERE name = \'" + name + "\'");
             rs.next();
             if(rs.getInt(3)>0) enough = true;
-            else enough = false;
+                else enough = false;
             return enough?"This advertieser has enough credits":"This advertiser does not have enough credits";
+        }
+        @RequestMapping("/api/delete")
+        public String delteAdvertiser(@RequestParam(name = "name")String name) throws Exception{
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test","sa","");
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("DELETE FROM advertisers WHERE name = \'" + name + "\'");
+            return name+ " deleted.";
         }
 
 
