2904c683bb4684815306cdb584207afca1dc7639
Cyrus Mahdavi <cyrusmahdavi@mikes-mbp-2.ccu.clearchannel.com>
2019-06-10 09:57:30 -0500

    swagger-ui working

--- src/main/java/ads/Config/SwaggerConfig.java
+++ src/main/java/ads/Config/SwaggerConfig.java
@@ -1,23 +1,24 @@
 package ads.Config;
 
-
-import java.net.URL;
-
-import org.springframework.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
-@ComponentScan(basePackages = {"com.company.project"})
-public class SwaggerConfig
-{
+public class SwaggerConfig {
     @Bean
     public Docket api() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.cloud")))
-                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.data.rest.webmvc")))
+                .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
